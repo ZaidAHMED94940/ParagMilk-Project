a2d48f85375b25a169b0e0f536d19288cb3c1d4c
ZaidAHMED94940 <zaid94940@gmail.com>
2025-03-05 20:09:08 +0530

    updated file

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app=express();
 const dotenv=require("dotenv");
 const connectDB = require("./db");
 const SubscriptionRoutes=require('./routes/Subscription')
+const sequelize=require("./sqldb.js")
+const sqlControl=require("./controller/MySqlSubscription.js")
 
 dotenv.config();
 const PORT=process.env.PORT
 app.use(express.json());
-
 connectDB();
+app.use("/api/sql",sqlControl)
 app.use('/api/v1',SubscriptionRoutes)
 
 
